75d6ace6d421a2d2e4838197be86c9224273ad5c
Massimo Nocentini <massimo.nocentini@gmail.com>
2025-01-23 09:39:05 +0100

    nodejs instructions

--- arch.sh
+++ arch.sh
@@ -1,9 +1,10 @@
 sudo pacman -S --needed base-devel git
-sudo pacman -S llvm clang openmp
+sudo pacman -S llvm clang openmp cmake lld
 sudo pacman -S vim piper rlwrap gnome-tweaks awesome-terminal-fonts discord mupdf mupdf-tools mpv
 sudo pacman -S curl wget cairo pango poppler libgit2 libssh2 libtool tree-sitter tree-sitter-cli nodejs gdk-pixbuf2 gtk4 glib2-devel
 sudo pacman -S sdl2 sdl2_gfx sdl2_image sdl2_mixer sdl2_net sdl2_ttf
 
+# for node I've used the instructions at https://nodejs.org/en/download/current
 
 # for activating services
 sudo systemctl enable gdm.service
